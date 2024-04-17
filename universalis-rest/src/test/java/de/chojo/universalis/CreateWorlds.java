e898445d55b1e34ec867f2b816c8a16cf9c5acab
Lilly Tempest <46890129+rainbowdashlabs@users.noreply.github.com>
2024-04-17 11:13:08 +0200

    Switch to LGPL 3.0 or later (#97)

--- universalis-rest/src/test/java/de/chojo/universalis/CreateWorlds.java
+++ universalis-rest/src/test/java/de/chojo/universalis/CreateWorlds.java
@@ -1,7 +1,7 @@
 /*
- *     SPDX-License-Identifier: AGPL-3.0-only
+ *     SPDX-License-Identifier: LGPL-3.0-or-later
  *
- *     Copyright (C) Rainbowdashlabs and Contributor
+ *     Copyright (C) RainbowDashLabs and Contributor
  */
 
 package de.chojo.universalis;
