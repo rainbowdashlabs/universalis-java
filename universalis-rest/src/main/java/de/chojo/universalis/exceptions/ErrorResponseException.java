62cdfcd2b236e507c1c5d29f9ff27d83b2df9717
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-04-17 12:20:08 +0200

    Cleanup code

--- universalis-rest/src/main/java/de/chojo/universalis/exceptions/ErrorResponseException.java
+++ universalis-rest/src/main/java/de/chojo/universalis/exceptions/ErrorResponseException.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ *     Copyright (C) RainbowDashLabs and Contributor
+ */
+
 package de.chojo.universalis.exceptions;
 
 public class ErrorResponseException extends ResponseException {
